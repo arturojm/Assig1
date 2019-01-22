067408062ae7504af1184e393481a77f4ce41137
arturo jimenez <arturo.jimenezucv@gmail.com>
2019-01-22 16:50:46 -0500

    Adding basic behaviour

--- dice.py
+++ dice.py
@@ -1,1 +1,10 @@
 from random import *
+
+opt = raw_input('Would you like to roll the dice? Y/N:  ')
+if(opt.lower() == 'y'):
+print ("Dice result: %s \n") % randrange(1,6)
+elif (opt.lower() == 'n'):
+print ("Good Bye!!")
+aux = False
+else:
+print ('Try again... \n')
