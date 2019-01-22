99726def7ab02b2f82458ac7a8b50e6ba46159ad
arturo jimenez <arturo.jimenezucv@gmail.com>
2019-01-22 16:51:42 -0500

    Adding LOOP behaviour

--- dice.py
+++ dice.py
@@ -1,10 +1,14 @@
 from random import *
 
-opt = raw_input('Would you like to roll the dice? Y/N:  ')
-if(opt.lower() == 'y'):
-print ("Dice result: %s \n") % randrange(1,6)
-elif (opt.lower() == 'n'):
-print ("Good Bye!!")
-aux = False
-else:
-print ('Try again... \n')
+aux = True
+
+while(aux):
+  opt = raw_input('Would you like to roll the dice? Y/N:  ')
+  if(opt.lower() == 'y'):
+    print ("Dice result: %s \n") % randrange(1,6)
+  elif (opt.lower() == 'n'):
+    print ("Good Bye!!")
+    aux = False
+  else:
+    print ('Try again... \n')
+exit()
